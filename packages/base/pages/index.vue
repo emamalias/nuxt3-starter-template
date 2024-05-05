18ffa651ed538ad590f15896164fa194a591cdfc
Elias Mamalias <mamalias23@gmail.com>
2024-05-05 18:50:23 +0800

    base setup

---
+++ packages/base/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+      <template #default>
+        <h1>Welcome</h1>
+      </template>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+
+useSeoMeta({
+  title: 'Nuxt3 Base App',
+  description: ''
+});
+</script>
---
+++ packages/base/pages/login.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLayout>
+      <template #default>
+        <h1>Login</h1>
+      </template>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+
+useSeoMeta({
+  title: 'Login',
+  description: ''
+});
+</script>
